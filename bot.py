b9cce7cca8b5a1c6d8926e928c249c0812bde80b
Sidney Bovet <sidney.bovet@gmail.com>
2016-12-12 11:51:14 +0100

    Adds repo addres upon 'git' command

--- bot.py
+++ bot.py
@@ -14,7 +14,11 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
+def repo(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="My source code is located at https://github.com/SidneyBovet/WakeUpBot")
+
 start_handler = CommandHandler('start', start)
+start_handler = CommandHandler('git', repo)
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
